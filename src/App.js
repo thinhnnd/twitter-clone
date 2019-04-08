d80ddb1e448fdfce30a509e730f35ee1f95b14b6
Thinh NND <16521179@gm.uit.edu.vn>
2019-04-08 14:43:59 +0700

    register component, redux for register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 import Home from './components/Home'
 import Login from './components/Auth/Login'
@@ -9,17 +12,19 @@ import Main from './components/Layout/Main'
 class App extends Component {
   render() {
     return (
-      <div>
-        <BrowserRouter>
-          <Main>
-            <Switch>
-              <Route exact path="/" component = {Home} />
-              <Route path="/login" component = {Login} />
-              <Route path="/register" component = {Register} />
-            </Switch> 
-          </Main>
-        </BrowserRouter>          
-      </div>
+      <Provider store={store}>
+        <div>
+          <BrowserRouter>
+            <Main>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+              </Switch>
+            </Main>
+          </BrowserRouter>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -3,11 +3,15 @@ import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {withStyles} from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import { registerUser } from '../../actions/authActions'
 
 const styles = {
     textField: {
         width: '100%',
-        marginBottom: 5
+        marginBottom: 5 
     },
     btnBlock: {
         textAlign: 'center',
@@ -45,6 +49,7 @@ class Register extends Component {
         }
 
         console.log(userData)
+        this.props.registerUser(userData, this.props.history)
     }
 
     render() {
@@ -96,4 +101,8 @@ class Register extends Component {
     }
 }
 
-export default withStyles(styles)(Register);
+const mapStateToProps = (state) => ({
+    errors: state.errors
+})
+
+export default connect(mapStateToProps, { registerUser })(withRouter(withStyles(styles)(Register)))
